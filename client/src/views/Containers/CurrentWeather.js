60323fd7d739695bae1e51af853d1e02cf1e2392
liuqi0328 <lqyterrible@gmail.com>
2018-11-10 22:48:49 -0500

    Updated home section and debugged location of the api

--- client/src/views/Containers/CurrentWeather.js
+++ client/src/views/Containers/CurrentWeather.js
@@ -1,94 +1,219 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
 
-import { weatherIcon } from '../../utils';
-import * as actions from '../../store/actions/index';
-import { WeatherCardError, Loader } from '../Components/index';
+// Assets
+import ThunderStormIcon from '../assets/weather_icons/01W.svg';
+import RainIcon from '../assets/weather_icons/02W.svg';
+import SnowIcon from '../assets/weather_icons/03W.svg';
+import ClearIcon from '../assets/weather_icons/04W-DAY.svg';
+import CloudsIcon from '../assets/weather_icons/05W.svg';
+import NoLocationFound from '../assets/no-location.svg';
+import LoadingIcon from '../assets/loading.svg';
 
 class CurrentWeather extends React.Component {
   constructor(props) {
     super(props);
 
-    props.actions.getWeatherData({ weatherSearchData: this.props.location.state });
+    this.state = ({
+      isLoading: true,
+      currentTemp: '',
+      humidity: '',
+      wind: '',
+      windDirection: '',
+      currentCondition: '',
+      currentConditionDescription: '',
+      weatherIcon: '',
+      cityName: '',
+      cityNotFound: '',
+    });
+  }
+
+  componentDidMount() {
+    // eslint-disable-next-line no-undef
+    fetch('/search-location-weather')
+      .then(res => res.json())
+      .then(data => {
+        if (data.data.cod === '404') {
+          this.setState({
+            isLoading: false,
+            cityNotFound: '404',
+          });
+        } else {
+          // Determine weather icon
+          let weatherId = data.data.weather[0].id;
+
+          if (weatherId <= 232) {
+            this.setState({ weatherIcon: ThunderStormIcon });
+          } else if (weatherId >= 300 && weatherId <= 531) {
+            this.setState({ weatherIcon: RainIcon });
+          } else if (weatherId >= 600 && weatherId <= 622) {
+            this.setState({ weatherIcon: SnowIcon });
+          } else if (weatherId === 800) {
+            this.setState({ weatherIcon: ClearIcon });
+          } else if (weatherId >= 801 && weatherId <= 804) {
+            this.setState({ weatherIcon: CloudsIcon });
+          }
+          this.setState({
+            isLoading: false,
+            currentTemp: Math.round(data.data.main.temp) + '°',
+            humidity: data.data.main.humidity + '%',
+            wind: Math.round(data.data.wind.speed) + ' mph',
+            windDirection: data.data.wind.deg,
+            currentCondition: data.data.weather[0].main,
+            currentConditionDescription: data.data.weather[0].description,
+            cityName: data.data.name,
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
-    const { loading, loaded, error, currentTemp, currentConditionDescription,
-      humidity, wind, cityName, weatherId } = this.props;
-
-    if (error) {
-      return (
-        <div>
-          <WeatherCardError />
+    const WeatherCardError = (
+      <div className='weatherCardContainer'>
+        <div className='weatherCardError'>
+          <img src={NoLocationFound} alt='no location found' />
+          <p> Whoa! Looks like there was an error with your zipcode.</p>
+          <Link to='/'><button>Try Again</button></Link>
         </div>
-      );
-    }
+      </div>
+    );
 
-    if (loading) {
-      return (
-        <Loader />
-      );
-    }
+    const WeatherConditions = (
+      this.state.cityNotFound === 404 ? <div> {WeatherCardError} </div> :
+        <div>
+          <div className='homeBtn'>
+            <Link to='/'><button>Home</button></Link>
+          </div>
+          <div className='weatherCardContainer'>
+            <div className='weatherCard'>
+              <img src={this.state.weatherIcon} alt='Weather icon' />
+              <div className='conditionsOverview'>
+                <p>{this.state.currentTemp}</p>
+                <p>{this.state.currentConditionDescription}</p>
+              </div>
+              <div className='conditionDetails'>
+                <p>Humidity: {this.state.humidity} </p>
+                <p>Wind Speed: {this.state.wind} </p>
+              </div>
+            </div>
+            <h4> Location | {this.state.cityName} </h4>
+          </div>
+        </div>
+    );
+
+    const LoadingDisplay = (
+      <div className='loading'>
+        <img className='loadingIcon' src={LoadingIcon} alt='loading icon' />
+      </div>
+    );
+
+    const CurrentWeatherCard = (
+      this.state.isLoading === true ? <div> {LoadingDisplay} </div> : <div> {WeatherConditions} </div>
+    );
 
     return (
       <div>
-        { loaded &&
-          (
-            <div>
-              <div className='homeBtn'>
-                <Link to='/'><button>Home</button></Link>
-              </div>
-              <div className='weatherCardContainer'>
-                <div className='weatherCard'>
-                  <img src={weatherIcon(weatherId)} alt='Weather icon'/>
-                  <div className='conditionsOverview'>
-                    <p>{currentTemp}°</p>
-                    <p>{currentConditionDescription}</p>
-                  </div>
-                  <div className='conditionDetails'>
-                    <p>Humidity: {humidity}% </p>
-                    <p>Wind Speed: {wind} mph </p>
-                  </div>
-                </div>
-
-                <h4> Location | {cityName} </h4>
-              </div>
-            </div>
-          )
-        }
+        {CurrentWeatherCard}
       </div>
     );
   }
 }
 
-const mapStateToProps = function(state) {
-  const { main, weather, name, wind, loading, loaded, error } = state;
+export default CurrentWeather;
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
 
-  return {
-    ...state,
-    loading: loading,
-    loaded: loaded,
-    error: error,
-    cityName: name,
-    weatherId: weather && weather[0].id,
-    humidity: main && main.humidity,
-    wind: wind && wind.speed,
-    windDirection: wind && wind.deg,
-    currentTemp: main && Math.round(main.temp),
-    currentCondition: weather && weather[0].main,
-    currentConditionDescription: weather && weather[0].description,
-  };
-};
+// import { weatherIcon } from '../../utils';
+// import * as actions from '../../store/actions/index';
+// import { WeatherCardError, Loader } from '../Components/index';
 
-const mapDispatchToProps = function(dispatch) {
-  return {
-    actions: {
-      getWeatherData: function(weatherSearchData) {
-        dispatch(actions.getWeatherData(weatherSearchData));
-      },
-    },
-  };
-};
+// class CurrentWeather extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-export default connect(mapStateToProps, mapDispatchToProps)(CurrentWeather);
+//     props.actions.getWeatherData({ weatherSearchData: this.props.location.state });
+//   }
+
+//   render() {
+//     const { loading, loaded, error, currentTemp, currentConditionDescription,
+//       humidity, wind, cityName, weatherId } = this.props;
+
+//     if (error) {
+//       return (
+//         <div>
+//           <WeatherCardError />
+//         </div>
+//       );
+//     }
+
+//     if (loading) {
+//       return (
+//         <Loader />
+//       );
+//     }
+
+//     return (
+//       <div>
+//         { loaded &&
+//           (
+//             <div>
+//               <div className='homeBtn'>
+//                 <Link to='/'><button>Home</button></Link>
+//               </div>
+//               <div className='weatherCardContainer'>
+//                 <div className='weatherCard'>
+//                   <img src={weatherIcon(weatherId)} alt='Weather icon'/>
+//                   <div className='conditionsOverview'>
+//                     <p>{currentTemp}°</p>
+//                     <p>{currentConditionDescription}</p>
+//                   </div>
+//                   <div className='conditionDetails'>
+//                     <p>Humidity: {humidity}% </p>
+//                     <p>Wind Speed: {wind} mph </p>
+//                   </div>
+//                 </div>
+
+//                 <h4> Location | {cityName} </h4>
+//               </div>
+//             </div>
+//           )
+//         }
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = function(state) {
+//   const { main, weather, name, wind, loading, loaded, error } = state;
+
+//   return {
+//     ...state,
+//     loading: loading,
+//     loaded: loaded,
+//     error: error,
+//     cityName: name,
+//     weatherId: weather && weather[0].id,
+//     humidity: main && main.humidity,
+//     wind: wind && wind.speed,
+//     windDirection: wind && wind.deg,
+//     currentTemp: main && Math.round(main.temp),
+//     currentCondition: weather && weather[0].main,
+//     currentConditionDescription: weather && weather[0].description,
+//   };
+// };
+
+// const mapDispatchToProps = function(dispatch) {
+//   return {
+//     actions: {
+//       getWeatherData: function(weatherSearchData) {
+//         dispatch(actions.getWeatherData(weatherSearchData));
+//       },
+//     },
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CurrentWeather);
